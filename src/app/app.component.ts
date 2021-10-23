327fd9d6ea6f0cc1631b5c2afa6b595e1ad4ae83
eastcoastdeveloper <uxdeveloper@protonmail.com>
2021-10-22 22:30:43 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import * as chartData from './data.json';
 
